1f4099b8cb66923221170e03855ad1e05ace7370
Somiah Aldouis <somiahjamal1997@gmail.com>
2020-12-15 04:03:38 +0300

    Initial commit

---
+++ app/src/main/java/somiah/jad/nerdlauncher/MainActivity.kt
@@ -0,0 +1,19 @@
+package somiah.jad.nerdlauncher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.app_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+}
